53f60b3d3755ef7ec1b2815b1931f08be1ef60ad
girin-sj <radianttwilight24@gmail.com>
2024-03-21 20:52:54 +0900

    font added

--- Reborn/theme/textstyle.js
+++ Reborn/theme/textstyle.js
@@ -1,9 +1,11 @@
 import { StyleSheet } from "react-native";
 import { colors } from "./colors";
 
+// For use fonts, write like this
+// >> fontFamily: '(font name)',
 export const textstyle = StyleSheet.create({
     Titletext: {
-       
+        fontFamily: 'NanumSquareB',
     },
     context:{
 
