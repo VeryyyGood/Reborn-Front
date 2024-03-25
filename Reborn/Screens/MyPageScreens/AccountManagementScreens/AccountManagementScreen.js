3ba7fed18b3e88e18c869a63ee7526b5cb42cdc4
mcy0325 <mcy325@naver.com>
2024-03-25 15:40:45 +0900

    0325 design

--- Reborn/Screens/MyPageScreens/AccountManagementScreens/AccountManagementScreen.js
+++ Reborn/Screens/MyPageScreens/AccountManagementScreens/AccountManagementScreen.js
@@ -1,13 +1,65 @@
 import React from "react";
-import {View, Text, StyleSheet, Image, TouchableOpacity} from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 
-
-const AccountManagementScreen = ({navigation: { navigate }} ) => (
-    <View>
-    <TouchableOpacity onPress={() => navigate('AccountModify')}>
+const AccountManagementScreen = ({ navigation: { navigate }} ) => (
+  <View style={styles.container}>
+    <View style={styles.imageWrapper}>
+      <Image source={require('../../../Assets/icons/profile_bg.png')} style={styles.backgroundImage}></Image>
+      <Image source={require('../../../Assets/icons/profile.png')} style={styles.profileImage}></Image>
+    </View>
+    <Text style={styles.fontBold}>김보경</Text>
+    <TouchableOpacity onPress={() => navigate('AccountModify')} style={styles.touchableStyle}>
       <Text>계정 정보 수정</Text>
+    </TouchableOpacity>
+    <Text style={styles.fontNormal}>
+      <Text>Email: </Text>
+      <Text>animalLove@gmail.com</Text>
+    </Text>
+    <Text style={styles.fontNormal}>
+      <Text>Since: </Text>
+      <Text>2024.01.24</Text>
+    </Text>
+    <TouchableOpacity onPress={() => navigate('AccountModify')} style={styles.touchableStyle}>
+      <Text>로그아웃</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => navigate('AccountModify')} style={styles.touchableStyle}>
+      <Text>계정 삭제하기</Text>
     </TouchableOpacity>
   </View>
 );
 
 export default AccountManagementScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  imageWrapper: {
+    position: 'relative', // 이 부모 View에 대해 자식 요소들을 상대적 위치로 설정
+    height: 100, // 또는 실제 이미지 크기에 맞게 조정
+    width: 100, // 또는 실제 이미지 크기에 맞게 조정
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  backgroundImage: {
+    position: 'absolute', // 절대 위치를 사용하여 다른 요소와 겹치도록 설정
+  },
+  profileImage: {
+    position: 'absolute', // 절대 위치를 사용하여 다른 요소와 겹치도록 설정
+    top: 50,
+  },
+  touchableStyle: {
+    marginTop: 30, // 이미지와 버튼 사이의 간격 조정
+    marginBottom: 30,
+  },
+  fontBold: {
+    marginTop: 65,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  fontNormal: {
+    fontSize: 16,
+  },
+});
--- Reborn/Screens/MyPageScreens/ReviewScreens/ReviewMainScreen.js
+++ Reborn/Screens/MyPageScreens/ReviewScreens/ReviewMainScreen.js
@@ -1,25 +1,87 @@
 import React from "react";
-import {View, Text, StyleSheet, Image, TouchableOpacity} from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { colors } from '../../../theme/colors';
 
-
-const ReviewMainScreen = ({navigation: { navigate }} ) => (
-    <View>
-    <TouchableOpacity onPress={() => navigate('ReviewStack', {screen: 'ReviewReconnect' })}>
-      <Text>리커넥트</Text>
-    </TouchableOpacity>
-    <TouchableOpacity onPress={() => navigate('ReviewStack', {screen: 'ReviewRemind' })}>
-      <Text>리마인드</Text>
-    </TouchableOpacity>
-    <TouchableOpacity onPress={() => navigate('ReviewStack', {screen: 'ReviewReveal' })}>
-      <Text>리빌</Text>
-    </TouchableOpacity>
-    <TouchableOpacity onPress={() => navigate('ReviewStack', {screen: 'ReviewRemember' })}>
-      <Text>리멤버</Text>
-    </TouchableOpacity>
-    <TouchableOpacity onPress={() => navigate('ReviewStack', {screen: 'ReviewReborn' })}>
-      <Text>리본</Text>
-    </TouchableOpacity> 
+const ReviewMainScreen = ({ navigation: { navigate }} ) => (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => navigate('ReviewStack', {screen: 'ReviewReconnect' })} style={styles.touchable}>
+        <View style={styles.imageTextContainer}>
+          <Image source={require('../../../Assets/icons/review_album.png')} />
+          <Text style={styles.imageText}>
+            <Text style={styles.reColor}>RE</Text>CONNECT:
+          </Text>
+          <Text style={styles.font}>나의 반려동물과 만나기</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigate('ReviewStack', {screen: 'ReviewRemind' })}>
+        <View style={styles.imageTextContainer}>
+          <Image source={require('../../../Assets/icons/review_album.png')} />
+          <Text style={styles.imageText}>
+            <Text style={styles.reColor}>RE</Text>MIND:
+          </Text>
+          <Text style={styles.font}>충분한 대화 나누기</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigate('ReviewStack', {screen: 'ReviewReveal' })}>
+        <View style={styles.imageTextContainer}>
+          <Image source={require('../../../Assets/icons/review_album.png')} />
+          <Text style={styles.imageText}>
+            <Text style={styles.reColor}>RE</Text>VEAL:
+          </Text>
+          <Text style={styles.font}>나의 감정 들여다보기</Text>
+        </View>
+      </TouchableOpacity>
+        <View style={styles.imageTextContainer}>
+          <Image source={require('../../../Assets/icons/review_album.png')} />
+          <Text style={styles.imageText}>
+            <Text style={styles.reColor}>RE</Text>MEMBER:
+          </Text>
+          <Text style={styles.font}>건강한 작별 준비하기</Text>
+        </View>
+      <TouchableOpacity onPress={() => navigate('ReviewStack', {screen: 'ReviewReborn' })}>
+        <View style={styles.imageTextContainer}>
+          <Image source={require('../../../Assets/icons/review_album.png')} />
+          <Text style={styles.imageText}>
+            <Text style={styles.reColor}>RE</Text>BORN:
+          </Text>
+          <Text style={styles.font}>나의 반려동물과 작별하기</Text>
+        </View>
+      </TouchableOpacity> 
   </View>
 );
 
 export default ReviewMainScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  touchable: {
+    alignItems: 'center', // TouchableOpacity 내부 요소를 가운데 정렬
+  },
+
+  imageTextContainer: {
+    position: 'relative', // 이미지와 텍스트 컨테이너
+    alignItems: 'flex-start', // 이미지와 텍스트를 가운데 정렬
+    marginTop: 20,
+  },
+
+  imageText: {
+    position: 'absolute', // 텍스트를 이미지 위에 절대적 위치로 설정
+    fontWeight: 'bold', // 텍스트 굵기
+    fontSize: 24,
+    padding: 18,
+  },
+
+  font: {
+    position: 'absolute',
+    fontSize: 16,
+    marginTop: 55,
+    marginLeft: 18,
+  },
+
+  reColor: {
+    color: colors.palette.Brown,
+  },
+});
