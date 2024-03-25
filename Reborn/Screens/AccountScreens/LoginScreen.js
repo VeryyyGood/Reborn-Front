4d42f3ab53632c71de877c0c9d56b166e01fb668
mcy0325 <mcy325@naver.com>
2024-03-25 23:54:15 +0900

    0325 design 4

--- Reborn/Screens/AccountScreens/LoginScreen.js
+++ Reborn/Screens/AccountScreens/LoginScreen.js
@@ -1,11 +1,50 @@
 import React from "react";
 import {View, Text, StyleSheet, Image, TouchableOpacity} from "react-native";
-
+import { buttonStyles } from '../../components';
+import { colors } from '../../theme';
 
 const LoginScreen = ({navigation: { navigate }} ) => (
-    <TouchableOpacity onPress={()=>navigate('Tabs', { screen: 'main' })}>
-        <Text>로그인 메인</Text>
-    </TouchableOpacity >
+    <View style={styles.container}>
+        <View style={styles.introContainer}>
+            <Text style={styles.introTitle}>PET <Text style={{color: colors.palette.Yellow}}>RE</Text>BORN,{'\n'}로그인 하기</Text>
+        </View>
+        <View style={styles.contentContainer}>
+            <Image style={{width: 470, height: 470, bottom: '10%'}} source={require('../../Assets/icons/app_icon.png')}/>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity style={buttonStyles.buttonLogin} onPress={()=>navigate('Tabs', { screen: 'main' })}>
+                    <Text style={styles.buttonText}>네이버 아이디로 로그인 </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    </View>
 );
 
 export default LoginScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.palette.White,
+    },
+    introContainer: {
+        marginTop: 30,
+    },
+    contentContainer: {
+        alignItems: 'center',
+    },
+    introTitle: {
+        fontSize: 45,
+        textAlign: 'left',
+        paddingLeft: 20,
+        fontWeight: 'bold',
+    },
+    buttonContainer: {
+        position: 'absolute',
+        top: '85%',
+        bottom: '20%',
+    },
+    buttonText: {
+        textAlign: 'center', // 버튼 텍스트 가운데 정렬
+        color: colors.palette.White,
+    },
+});
--- Reborn/components/buttonStyles.ts
+++ Reborn/components/buttonStyles.ts
@@ -22,4 +22,14 @@ export const buttonStyles = StyleSheet.create({
         borderRadius: 30,
         height: 50,
     },
+    buttonLogin: {
+        backgroundColor: colors.palette.NaverGreen,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 30,
+        marginVertical: 20,
+        marginBottom: 30,
+        borderRadius: 16,
+        height: 50,
+    }
 }); //지금 버튼이 padding으로 여백을 주고 있는데 뷰에 여백을 주는게 아니라 버튼에 줘서 버튼이 일정한 크기 유지를 안하고 글자 수에 따라 움직임 근데 일정하게하려면 px를 직접 줘야할텐데 그럼 화면커지면 이상할듯
