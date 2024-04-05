0f4f62df8a8e6742916df8523e19104e8f34d8b1
girin-sj <radianttwilight24@gmail.com>
2024-04-05 20:33:39 +0900

    add back to main button

--- Reborn/Navigation/RebornDogStack.tsx
+++ Reborn/Navigation/RebornDogStack.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
+import styled from "styled-components/native";
 import RemindIntroScreen from "../Screens/RebornScreens/dog/RemindIntroScreen";
 import FeedScreen from "../Screens/RebornScreens/dog/FeedScreen";
 import PetScreen from "../Screens/RebornScreens/dog/PetScreen";
 import SnackScreen from "../Screens/RebornScreens/dog/SnackScreen";
 import WalkScreen from "../Screens/RebornScreens/dog/WalkScreen";
+import { Image } from "react-native";
 
 const NativeRebornDogStack = createNativeStackNavigator();
 
-const RebornDogStack = () => (
+const RebornDogStack = ({ navigation: { navigate } }) => (
   <NativeRebornDogStack.Navigator
-    screenOptions={{ headerTitleAlign: "center" }}
+    screenOptions={({ navigation }) => ({
+      headerTitleAlign: "center",
+      headerLeft: () => (
+        <ImageButton
+          onPress={() => navigation.navigate("Main")}
+        />
+      ),
+    })}
   >
     <NativeRebornDogStack.Screen
       name="Intro"
@@ -42,3 +50,15 @@ const RebornDogStack = () => (
 );
 
 export default RebornDogStack;
+
+const GoToMainButton = styled.Pressable`
+  margin-left: 3%;
+`;
+const ImageButton = ({ onPress }) => {
+  return(
+  <GoToMainButton onPress={onPress}>
+    <Image
+      source={require('../Assets/icons/tabIcons/back.png')}
+    />
+  </GoToMainButton>
+);}
--- Reborn/Navigation/Tabs.js
+++ Reborn/Navigation/Tabs.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Text,View } from "react-native";
+import { Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { colors } from "../theme";
 
-import TestIntroScreen from '../Screens/SelfTestScreens/TestIntroScreen';
-import MainScreen from '../Screens/MainScreen';
-import ShareBoardScreen from '../Screens/ShareboardScreens/MainShareScreen';
+import TestIntroScreen from "../Screens/SelfTestScreens/TestIntroScreen";
+import MainScreen from "../Screens/MainScreen";
+import ShareBoardScreen from "../Screens/ShareboardScreens/MainShareScreen";
 import RediaryMain from "../Screens/ReDiaryScreens/RediaryMainScreen";
 import MypageMainScreen from "../Screens/MyPageScreens/MypageMainScreen";
 
@@ -15,12 +15,33 @@ import ShareDrawers from "./ShareDrawer";
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => (
-    <Tab.Navigator screenOptions={{tabBarActiveTintColor: colors.palette.Brown, headerTitleAlign:"center"}}>
-        <Tab.Screen name='Main' component={MainScreen} options={{ tabBarLabel: "Home", headerShown: false ,}}/>
-        <Tab.Screen name='Shared' component={ShareDrawers} options={{ headerShown: false ,tabBarStyle: { display: 'none'},}} />
-        <Tab.Screen name='RediaryMain' component={RediaryMain} options={{ tabBarStyle: { display: 'none'}, title: "RE: Diary"}} />
-        <Tab.Screen name='MypageMain' component={MypageMainScreen} options={{ tabBarStyle: { display: 'none'}, title: "마이페이지"}} />
-    </Tab.Navigator>
-)
+  <Tab.Navigator
+    screenOptions={{
+      tabBarActiveTintColor: colors.palette.Brown,
+      headerTitleAlign: "center",
+    }}
+  >
+    <Tab.Screen
+      name="Main"
+      component={MainScreen}
+      options={{ tabBarLabel: "Home", headerShown: false }}
+    />
+    <Tab.Screen
+      name="Shared"
+      component={ShareDrawers}
+      options={{ headerShown: false, tabBarStyle: { display: "none" } }}
+    />
+    <Tab.Screen
+      name="RediaryMain"
+      component={RediaryMain}
+      options={{ tabBarStyle: { display: "none" }, title: "RE: Diary" }}
+    />
+    <Tab.Screen
+      name="MypageMain"
+      component={MypageMainScreen}
+      options={{ tabBarStyle: { display: "none" }, title: "마이페이지" }}
+    />
+  </Tab.Navigator>
+);
 
 export default Tabs;
