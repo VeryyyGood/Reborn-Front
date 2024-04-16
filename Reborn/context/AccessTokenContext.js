a9a5250515751391d7c3c39164de0df1ebfcbb9f
mcy0325 <mcy325@naver.com>
2024-04-16 15:03:38 +0900

    0416 login modify

---
+++ Reborn/context/AccessTokenContext.js
@@ -0,0 +1,15 @@
+import React, { createContext, useContext, useState } from "react";
+
+const AccessTokenContext = createContext();
+
+export const useAccessToken = () => useContext(AccessTokenContext);
+
+export const AccessTokenProvider = ({ children }) => {
+  const [accessToken, setAccessToken] = useState(null);
+
+  return (
+    <AccessTokenContext.Provider value={{ accessToken, setAccessToken }}>
+      {children}
+    </AccessTokenContext.Provider>
+  );
+};
