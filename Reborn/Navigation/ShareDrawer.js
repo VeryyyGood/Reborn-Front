5c931103744e97d9dbe05d0f6d7e3a04ff644f65
kimbodle <103980156+kimbodle@users.noreply.github.com>
2024-04-27 20:51:51 +0900

    itemShareScreen delate

--- Reborn/Navigation/ShareDrawer.js
+++ Reborn/Navigation/ShareDrawer.js
@@ -6,7 +6,6 @@ import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import MainShareScreen from '../Screens/ShareboardScreens/MainShareScreen';
 import EmotionShareScreen from '../Screens/ShareboardScreens/EmotionShareScreen';
 import ChatShareScreen from '../Screens/ShareboardScreens/ChatShareScreen';
-import ItemShareScreen from '../Screens/ShareboardScreens/ItemShareScreen';
 import VolunteerShareScreen from '../Screens/ShareboardScreens/VolunteerShareScreen';
 
 import ShareContentScreen from '../Screens/ShareboardScreens/ShareContentScreen';
@@ -50,7 +49,6 @@ const ShareDrawers = () => (
             <Drawer.Screen name='ShareMain' component={MainShareScreen} options={{drawerLabel: '전체 나눔', title: '전체 나눔 게시판'}}/>
             <Drawer.Screen name='Emotion' component={EmotionShareScreen} options={{drawerLabel: '감정 나눔', title: '감정 나눔 게시판'}}/>
             <Drawer.Screen name='Chat' component={ChatShareScreen} options={{drawerLabel: '담소 나눔',title: '담소 나눔 게시판'}}/>
-            <Drawer.Screen name='Item' component={ItemShareScreen} options={{drawerLabel: '물품 나눔', title: '물품 나눔 게시판' }}/>
             <Drawer.Screen name='Volunteer' component={VolunteerShareScreen} options={{drawerLabel: '봉사 나눔', title: '봉사 게시판'}}/>
             <Drawer.Screen name='ShareContent' component={ShareContentScreen} options={{title: '나눔 게시판', drawerItemStyle: { height: 0 }}}/>
             <Drawer.Screen name='ShareWrite' component={ShareWriteScreen} options={{headerShown: false, drawerItemStyle: { height: 0 }}}/>
