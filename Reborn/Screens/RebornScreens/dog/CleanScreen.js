3dd51cecb63e9525c104d905b20528f53a9b892e
girin-sj <radianttwilight24@gmail.com>
2024-05-03 16:46:44 +0900

    RE:MEMBER completed

--- Reborn/Screens/RebornScreens/dog/CleanScreen.js
+++ Reborn/Screens/RebornScreens/dog/CleanScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useRef } from "react";
+import React, { useState, useContext, useRef, useEffect } from "react";
 import {
   Text,
   ImageBackground,
@@ -11,50 +11,70 @@ import { colors } from "../../../theme";
 import { textStyles, ButtonBrownBottom } from "../../../components";
 import { TutorialModal } from "../../../components";
 import AppContext from "./AppContext";
+import { useFocusEffect } from "@react-navigation/native";
 
 import dog_boxURL from "../../../Assets/Images/dog/dog_box.png";
 
 import dog_bathURL from "../../../Assets/Images/dog/dog_bath.png";
-import dog_padURL from "../../../Assets/Images/dog/dog_bowl_no.png"; // need modifing
+import dog_padURL from "../../../Assets/Images/dog/dog_pad.png";
 import dog_bowlURL from "../../../Assets/Images/dog/dog_bowl.png";
 import dog_cushionURL from "../../../Assets/Images/dog/dog_cushion.png";
 import dog_snackURL from "../../../Assets/Images/dog/dog_snack.png";
 import dog_toyURL from "../../../Assets/Images/dog/dog_toy.png";
+import { isClassDeclaration } from "typescript";
 
 const CleanScreen = ({ navigation: { navigate } }) => {
   const myContext = useContext(AppContext);
+  const [isCleaned, setIsCleaned] = useState(0);
+  const [isNextDayButtonVisible, setIsNextDayButtonVisible] = useState(false);
+  const [modalVisible, setModalVisible] = useState(true);
 
-  const stuffArray = [
-    { key: "bath", value: false },
-    { key: "pad", value: false },
-    { key: "bowl", value: false },
-    { key: "snack", value: false },
-    { key: "cushion", value: false },
-    { key: "toy", value: false },
+  const ModalTextArray = [
+    `오늘부터 3일간 반려동물 용품을 정리해보려고 합니다. 무엇을 먼저 정리해야 할지 고민이 되는 분들을 위해 하루에 두 용품씩 가이드를 제공합니다.\n정리가 끝난 항목을 박스쪽으로 드래그 해주세요. 오늘은 목욕용품과 위생용품을 정리해봅시다.`,
+    `오늘은 식기와 식품을 정리해봅시다. 정리가 끝난 항목을 박스쪽으로 드래그 해주세요.`,
+    `오늘은 쿠션 등 생활용품과 장난감을 정리해봅시다. 정리가 끝난 항목을 박스쪽으로 드래그 해주세요.`,
   ];
 
-  const goalPositionArray = [[-110, 290, -95, 340]];
+  const [isDraggable, setIsDraggable] = useState({
+    bath: false,
+    pad: false,
+    bowl: false,
+    snack: false,
+    cushion: false,
+    toy: false,
+  });
 
-  if (myContext.contentsDay === 12) {
-    changeValue("bath", true);
-    changeValue("pad", true);
-  } else if (myContext.contentsDay === 13) {
-    changeValue("bowl", true);
-    changeValue("snack", true);
-  } else if (myContext.contentsDay === 14) {
-    changeValue("cushion", true);
-    changeValue("toy", true);
-  }
+  // bath, pad, bowl, snack, cushion, toy
+  const goalPositionArray = [
+    [-15, 165],
+    [-235, 165],
+    [-235, -95],
+    [-15, 55],
+    [-205, 15],
+    [-135, 285],
+  ];
 
-  const changeValue = (key, newValue) => {
-    const newDataArray = dataArray.map((item) => {
-      if (item.key === key) {
-        return { ...item, value: newValue };
-      }
-      return item;
-    });
-    setDataArray(newDataArray);
-  };
+  useEffect(() => {
+    console.log(isCleaned);
+    if (isCleaned === 2) {
+      setIsNextDayButtonVisible(true);
+    }
+    if (myContext.contentsDay === 12) {
+      setIsDraggable((prev) => ({ ...prev, bath: true, pad: true }));
+    } else if (myContext.contentsDay === 13) {
+      setIsDraggable((prev) => ({ ...prev, bowl: true, snack: true }));
+    } else if (myContext.contentsDay === 14) {
+      setIsDraggable((prev) => ({ ...prev, cushion: true, toy: true }));
+    }
+  }, [myContext.contentsDay, isCleaned]);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      setIsCleaned(0);
+      setModalVisible(true);
+      setIsNextDayButtonVisible(false);
+    }, [])
+  );
 
   return (
     <Container>
@@ -79,79 +99,152 @@ const CleanScreen = ({ navigation: { navigate } }) => {
           }}
           resizeMode="contain"
         />
-        <DraggableImage
-          source={dog_bathURL}
-          style={{
-            width: "50%",
-            height: "50%",
-            position: "absolute",
-            marginTop: "22%",
-          }}
-          resizeMode="contain"
-        />
-        <DraggableImage
-          source={dog_padURL}
-          style={{
-            width: "50%",
-            height: "50%",
-            position: "absolute",
-            marginLeft: "50%",
-            marginTop: "30%",
-          }}
-        />
-        <DraggableImage
-          source={dog_bowlURL}
-          style={{
-            width: "50%",
-            height: "50%",
-            position: "absolute",
-            marginLeft: "60%",
-            marginTop: "100%",
-          }}
-        />
-        <DraggableImage
-          source={dog_cushionURL}
-          style={{
-            width: "50%",
-            height: "50%",
-            position: "absolute",
-            marginLeft: "45%",
-            marginTop: "60%",
-          }}
-        />
-        <DraggableImage
-          source={dog_snackURL}
-          style={{
-            width: "50%",
-            height: "50%",
-            position: "absolute",
-            marginTop: "48%",
-          }}
-        />
-        <DraggableImage
-          source={dog_toyURL}
-          style={{
-            width: "50%",
-            height: "50%",
-            position: "absolute",
-            marginLeft: "23%",
-            marginTop: "5%",
-          }}
-          goalPositionArray={goalPositionArray[0]}
-        />
-        <ButtonBrownBottom
-          text={"쓰다듬으러 가기"}
-          onPress={() => {
-            navigate("Pet");
-          }}
-        />
+        {isDraggable.bath ? (
+          <DraggableImage
+            source={dog_bathURL}
+            style={{
+              width: "50%",
+              height: "50%",
+              position: "absolute",
+              marginTop: "22%",
+            }}
+            goalPositionArray={goalPositionArray[0]}
+            isDraggable={isDraggable.bath}
+            isCleaned={isCleaned}
+            setIsCleaned={setIsCleaned}
+          />
+        ) : (
+          ""
+        )}
+        {isDraggable.pad ? (
+          <DraggableImage
+            source={dog_padURL}
+            style={{
+              width: "60%",
+              height: "56%",
+              position: "absolute",
+              marginLeft: "50%",
+              marginTop: "30%",
+            }}
+            goalPositionArray={goalPositionArray[1]}
+            isDraggable={isDraggable.pad}
+            isCleaned={isCleaned}
+            setIsCleaned={setIsCleaned}
+          />
+        ) : (
+          ""
+        )}
+        {isDraggable.bowl ? (
+          <DraggableImage
+            source={dog_bowlURL}
+            style={{
+              width: "50%",
+              height: "50%",
+              position: "absolute",
+              marginLeft: "60%",
+              marginTop: "100%",
+            }}
+            goalPositionArray={goalPositionArray[2]}
+            isDraggable={isDraggable.bowl}
+            isCleaned={isCleaned}
+            setIsCleaned={setIsCleaned}
+          />
+        ) : (
+          ""
+        )}
+        {isDraggable.snack ? (
+          <DraggableImage
+            source={dog_snackURL}
+            style={{
+              width: "50%",
+              height: "50%",
+              position: "absolute",
+              marginTop: "48%",
+            }}
+            goalPositionArray={goalPositionArray[3]}
+            isDraggable={isDraggable.snack}
+            isCleaned={isCleaned}
+            setIsCleaned={setIsCleaned}
+          />
+        ) : (
+          ""
+        )}
+        {isDraggable.cushion ? (
+          <DraggableImage
+            source={dog_cushionURL}
+            style={{
+              width: "50%",
+              height: "50%",
+              position: "absolute",
+              marginLeft: "45%",
+              marginTop: "60%",
+            }}
+            goalPositionArray={goalPositionArray[4]}
+            isDraggable={isDraggable.cushion}
+            isCleaned={isCleaned}
+            setIsCleaned={setIsCleaned}
+          />
+        ) : (
+          ""
+        )}
+
+        {isDraggable.toy ? (
+          <DraggableImage
+            source={dog_toyURL}
+            style={{
+              width: "50%",
+              height: "50%",
+              position: "absolute",
+              marginLeft: "23%",
+              marginTop: "5%",
+            }}
+            goalPositionArray={goalPositionArray[5]}
+            isDraggable={isDraggable.toy}
+            isCleaned={isCleaned}
+            setIsCleaned={setIsCleaned}
+          />
+        ) : (
+          ""
+        )}
+        {isNextDayButtonVisible ? (
+          <ButtonBox>
+            <ButtonBrownBottom
+              text={"거실로 돌아가기"}
+              onPress={() => {
+                navigate("ReFinish");
+              }}
+            />
+          </ButtonBox>
+        ) : (
+          ""
+        )}
+        {modalVisible ? (
+          <TutorialModal
+            text={ModalTextArray[myContext.contentsDay - 12]}
+            modalStyles={modalVisible}
+            onPress={() => {
+              {
+                setModalVisible(false);
+              }
+            }}
+          />
+        ) : (
+          ""
+        )}
       </ImageBackground>
     </Container>
   );
 };
 export default CleanScreen;
 
-const DraggableImage = ({ source, style, goalPositionArray }) => {
+const DraggableImage = ({
+  source,
+  style,
+  goalPositionArray,
+  isDraggable,
+  isCleaned,
+  setIsCleaned,
+}) => {
   // Values
   const opacity = useRef(new Animated.Value(1)).current;
   const scale = useRef(new Animated.Value(1)).current;
@@ -185,37 +278,44 @@ const DraggableImage = ({ source, style, goalPositionArray }) => {
   // Pan Responders
   const panResponder = useRef(
     PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
-      onPanResponderMove: (_, { dx, dy }) => {
-        console.log({ dx, dy });
-        position.setValue({ x: dx, y: dy });
-      },
+      onStartShouldSetPanResponder: () => isDraggable,
+      onMoveShouldSetPanResponder: () => isDraggable,
+      onPanResponderMove: isDraggable
+        ? Animated.event([null, { dx: position.x, dy: position.y }], {
+            useNativeDriver: false,
+          })
+        : null,
       onPanResponderGrant: () => {
-        onPressIn.start();
+        if (isDraggable) {
+          // isDraggable?
+          onPressIn.start();
+        }
       },
-      onPanResponderRelease: (_, { dx, dy }) => {
+      onPanResponderRelease: (evt, { dx, dy }) => {
+        if (!isDraggable) return;
+        // console.log("Release event", dx, dy);
+        // is stuff's position near the box?
         if (
           dx > goalPositionArray[0] &&
           dy > goalPositionArray[1] &&
-          dx < goalPositionArray[2] &&
-          dy < goalPositionArray[3]
+          dx < goalPositionArray[0] + 60 &&
+          dy < goalPositionArray[1] + 60
         ) {
           console.log("Cleaned Up!!!");
+          setIsCleaned((isCleaned) => isCleaned + 1);
           Animated.sequence([
             Animated.parallel([onDropScale, onDropOpacity]),
-            Animated.timing(position, {
-              toValue: 0,
-              duration: 50,
-              easing: Easing.linear,
-              useNativeDriver: true,
-            }),
+            goHome,
           ]).start();
         } else {
-          Animated.parallel([onPressOut, goHome]).start();
+          onPressOut.start(() => {
+            goHome.start();
+          });
         }
       },
     })
   ).current;
+
   return (
     <Animated.Image
       {...panResponder.panHandlers}
@@ -224,6 +324,7 @@ const DraggableImage = ({ source, style, goalPositionArray }) => {
         style,
         {
           transform: [...position.getTranslateTransform(), { scale }],
+          opacity,
         },
       ]}
       resizeMode="center"
@@ -239,4 +340,8 @@ const Container = styled.View`
 const Box = styled.Image`
   width: 100%;
   height: 100%;
+`;
+
+const ButtonBox = styled.View`
+  margin: 130% 0% 0% 0%;
 `;
