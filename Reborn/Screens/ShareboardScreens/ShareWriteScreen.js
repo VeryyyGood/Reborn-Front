2f074ab55c5a6c35e8826ef9b6bef868f1becb11
kimbodle <103980156+kimbodle@users.noreply.github.com>
2024-05-06 20:41:13 +0900

    shareWriteScreen dropdown, textinput, main

--- Reborn/Screens/ShareboardScreens/ShareWriteScreen.js
+++ Reborn/Screens/ShareboardScreens/ShareWriteScreen.js
@@ -4,49 +4,86 @@ import {View, Text,Button,TouchableOpacity, TextInput, Image,StyleSheet} from "r
 import styled from "styled-components/native";
 import DropDownPicker from 'react-native-dropdown-picker';
 
-import { buttonStyles, textStyles } from "../../components";
+import { CompleteButton, buttonStyles, textStyles } from "../../components";
 import { colors } from "../../theme";
 
+import axios from "axios";
+import { useAccessToken, useGlobalNickname } from "../../context/AccessTokenContext";
+
 const ShareWriteScreen = ({navigation} ) => {
+    const { accessToken } = useAccessToken();
+
+    const [profileImage, setProfileImage] = useState(
+        require('../../Assets/icons/profile.png')
+      );
+
     const [open, setOpen] = useState(false);
     const [value, setValue] = useState(null);
     const [items, setItems] = useState([
         {label: '감정 나눔', value: '1'},
         {label: '담소 나눔', value: '2'},
-        {label: '물품 나눔', value: '3'},
-        {label: '봉사 나눔', value: '4'},
+        {label: '봉사 나눔', value: '3'},
     ]);
+
+    useEffect(() => {
+        getProfileImage();
+      }, []);
+    
+
+      const getProfileImage = async () => {
+        try {
+          const timestamp = new Date().getTime();
+          const profileImageResponse = await axios.get(
+            `http://reborn.persi0815.site/users/main?timestamp=${timestamp}`,
+            {
+              headers: {
+                Authorization: `Bearer ${accessToken}`,
+              },
+            }
+          );
+          console.log(profileImageResponse.data);
+          if(profileImageResponse.data.result.profileImage){
+              setProfileImage({ uri: profileImageResponse.data.result.profileImage });
+            }  
+        } catch (error) {
+          console.error("Profile image fetch error:", error);
+        }
+      };
+
     return (
-        <View style={{paddingHorizontal: 10, backgroundColor: colors.background}}>
-        <View style={styles.header}>
-            <TouchableOpacity onPress={()=> navigation.goBack()}>
-                <Image source={require('../../Assets/icons/ShareBoard/xicon.png')}/>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={()=> console.log(value)}>
-                <Image style={{width: 100, height: 100, resizeMode: 'contain'}} source={require('../../Assets/icons/ShareBoard/uploadbutton.png')} />
-            </TouchableOpacity>
-        </View> 
-        <View style={{}}>
-            <DropDownPicker
-            open={open}
-            value={value}
-            items={items}
-            setOpen={setOpen}
-            setValue={setValue}
-            setItems={setItems}
-            placeholder="카테고리"
-            listMode="MODAL"
-            modalProps={{
-                animationType: 'fade',
-            }}
-            modalTitle="선택해주세요."
-            />
+        <View style={{flex: 1, paddingHorizontal: 10, backgroundColor: colors.background}}>
+            <View>
+                <View style={styles.header}>
+                    <TouchableOpacity onPress={()=> navigation.goBack()}>
+                        <Image source={require('../../Assets/icons/ShareBoard/xicon.png')}/>
+                    </TouchableOpacity>
+                <View style={{marginVertical: '6%'}}>
+                    <CompleteButton text="작성완료" onPress={()=> console.log(value)}> </CompleteButton>
+                </View>
+                </View> 
+                <View style={{}}>
+                    <DropDownPicker
+                    open={open}
+                    value={value}
+                    items={items}
+                    setOpen={setOpen}
+                    setValue={setValue}
+                    setItems={setItems}
+                    placeholder="게시판을 선택해주세요"
+                    listMode="FLATLIST"
+                    modalProps={{
+                        animationType: 'fade',
+                    }}
+                    modalTitle="선택해주세요."
+                    //listItemContainerStyle={{}}
+                    />
+                </View>
+                <View style={{flexDirection:'row',}}>
+                    <Image style={{width: "15%", resizeMode: 'contain'}} source={profileImage}/>
+                        <TextInput style={{marginLeft: '3%', fontFamily: "Poppins-Bold", fontSize: 18, marginRight: 30, marginTop: '7%' }} multiline={true}>게시판 글쓰기</TextInput>
+                </View>
+            </View>
         </View>
-        <View style={{flexDirection:'row',}}>
-            <Image style={{width: "15%", resizeMode: 'contain'}} source={require('../../Assets/icons/profile.png')}/>
-                <TextInput style={{marginLeft: '3%', fontFamily: "Poppins-Bold", fontSize: 18}}>게시판 글쓰기</TextInput>
-        </View>
-    </View>
     );
 };
 
