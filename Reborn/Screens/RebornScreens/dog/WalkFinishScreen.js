c8687801f1ab2dc447bff08a9f9840c82565db2a
girin-sj <radianttwilight24@gmail.com>
2024-05-10 19:56:03 +0900

    RE:MIND connected with server

--- Reborn/Screens/RebornScreens/dog/WalkFinishScreen.js
+++ Reborn/Screens/RebornScreens/dog/WalkFinishScreen.js
@@ -1,12 +1,55 @@
-import React from "react";
+import React, { useContext } from "react";
 import styled from "styled-components/native";
 import { Text, ImageBackground } from "react-native";
 import { colors } from "../../../theme";
 import { ButtonBrownBottom, textStyles } from "../../../components";
+import AppContext from "./AppContext";
+import axios from "axios";
+
+import { useAccessToken } from "../../../context/AccessTokenContext";
 
 import dogimageURL from "../../../Assets/Images/dog/dog_idle.png";
 
 const WalkFinishScreen = ({ navigation: { navigate } }) => {
+  const { accessToken } = useAccessToken();
+  const myContext = useContext(AppContext);
+
+  const linkArray = [
+    "http://reborn.persi0815.site/reborn/remind/walk",
+    "http://reborn.persi0815.site/reborn/reveal/walk",
+    "http://reborn.persi0815.site/reborn/remember/walk",
+  ];
+
+  // RE:MIND & RE:VEAL & RE:MEMBER& RE:BORN what day? => Post Link
+  const handleLink = (day) => {
+    if (day >= 2 && day <= 6) {
+      return linkArray[0];
+    } else if (day >= 7 && day <= 11) {
+      return linkArray[1];
+    }
+    return linkArray[2];
+  };
+
+  const requestPostWalk = async () => {
+    try {
+      const response = await axios.post(
+        handleLink(myContext.contentsDay),
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      console.log(response.data);
+      return response; //함수에서 서버 응답 반환
+    } catch (error) {
+      //console.error("ERROR", error);
+      console.log("Error Response Body:", error.response.data);
+      throw error; //에러를 다시 던져서 외부에서 처리할 수 있게 함
+    }
+  };
+
   return (
     <Container>
       <ImageBackground
@@ -20,7 +63,9 @@ const WalkFinishScreen = ({ navigation: { navigate } }) => {
         <DogImage source={dogimageURL} resizeMode="center" />
         <ButtonBrownBottom
           text="간식주러 가기"
-          onPress={() => navigate("Snack")}
+          onPress={() => {
+            requestPostWalk(), navigate("Snack");
+          }}
         />
       </ImageBackground>
     </Container>
