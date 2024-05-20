62fc4327f44c80db0454f3888ed6fe85babf1d25
kimbodle <103980156+kimbodle@users.noreply.github.com>
2024-05-20 19:23:32 +0900

    Rediary Write, Result Post gooooooooooodddddddddddddddddddddddddddddddd

--- Reborn/Screens/ReDiaryScreens/RediaryWriteScreen.js
+++ Reborn/Screens/ReDiaryScreens/RediaryWriteScreen.js
@@ -1,24 +1,74 @@
 import React, { useState, useContext } from "react";
 import styled from "styled-components/native";
 import { colors } from "../../theme";
-import { GrayLine, CompleteButton, RadioButton } from "../../components";
-import { View, TextInput, StyleSheet } from "react-native";
-import axios from "axios";
 import { API_URL, CLIENT_ID, CLIENT_SECRET } from "@env";
+import {
+  GrayLine,
+  CompleteButton,
+  RadioButton,
+  Toast
+} from "../../components";
+import { View, TextInput,  StyleSheet } from "react-native";
+import { useFocusEffect } from "@react-navigation/native";
+
+import axios from "axios";
+
+import { useAccessToken } from "../../context/AccessTokenContext";
 
 import sunImage from "../../Assets/icons/rediaryimage/sun.png";
 import cloudImage from "../../Assets/icons/rediaryimage/cloud.png";
 import rainImage from "../../Assets/icons/rediaryimage/rain.png";
 
-const RediaryWriteScreen = ({ navigation: { navigate } }) => {
+const EmotionScreen = ({ navigation: { navigate } }) => {
+  const { accessToken } = useAccessToken();
+
   const [selectedEmotion, setSelectedEmotion] = useState(null);
+  const [showToast, setShowToast] = useState(false); // for Emotion
+  const [showToast_answer, setShowToast_answer] = useState(false); // for answer
   const [answer, onChangeAnswer] = React.useState("");
+  const [title, onChangeTitle] = React.useState("");
 
   const emotions = [
     { id: "SUNNY", image: sunImage },
     { id: "CLOUDY", image: cloudImage },
     { id: "RAINY", image: rainImage },
   ];
+
+  // refresh when Screen is focused
+  useFocusEffect(
+    React.useCallback(() => {
+      setSelectedEmotion(null);
+      onChangeAnswer("");
+      onChangeTitle("");
+    }, [])
+  );
+
+  // go to Result Page
+  const goToNextPage = async () => {
+    if (!selectedEmotion) {
+      setShowToast(true);
+    } else {
+      setShowToast(false);
+      if (answer && title) {
+        setShowToast_answer(false);
+        const analysisResult = await analyzeEmotion(answer);
+        if (analysisResult) {
+          console.log(analysisResult);
+          navigate("RediaryResult", {
+            answer,
+            selectedEmotion,
+            analysisResult,
+            title,
+          });
+        } else {
+          alert("감정 분석에 실패했습니다. 다시 시도해주세요.");
+        }
+      } else {
+        setShowToast_answer(true);
+        console.log(showToast_answer);
+      }
+    }
+  };
 
   const analyzeEmotion = async (text) => {
     try {
@@ -47,17 +97,23 @@ const RediaryWriteScreen = ({ navigation: { navigate } }) => {
           <RadioButton
             key={emotion.id}
             isSelected={selectedEmotion === emotion.id}
-            onPress={() => setSelectedEmotion(emotion.id)}
-            label={emotion.label}
+            onPress={() => {
+              setSelectedEmotion(emotion.id);
+              setShowToast(false);
+            }}
             image={emotion.image}
           />
         ))}
       </View>
-      <TitleText style={{ marginHorizontal: "8%" }}>감정일기</TitleText>
+      <TitleText style={{ marginHorizontal: "8%" }}>
+        감정일기
+      </TitleText>
       <View style={styles.titleText}>
         <TextInput
           placeholder="제목을 입력해주세요."
           style={styles.titleInput}
+          onChangeText={onChangeTitle}
+          value={title}
         />
       </View>
       <TextInputContainer>
@@ -69,24 +125,38 @@ const RediaryWriteScreen = ({ navigation: { navigate } }) => {
           multiline={true}
         ></TextInput>
       </TextInputContainer>
-      <CompleteButton
-        text="작성완료"
-        onPress={async () => {
-          const result = await analyzeEmotion(answer);
-          if (result) {
-            console.log(result.document.sentiment);
-          }
-        }}
-      />
+      <ToastContainer>
+        {showToast ? (
+          <Toast showToast={showToast} message="감정을 선택해주세요" />
+        ) : (
+          ""
+        )}
+      </ToastContainer>
+      <ToastContainer>
+        {showToast_answer ? (
+          <Toast showToast={showToast_answer} message="일기를 작성해주세요" />
+        ) : (
+          ""
+        )}
+      </ToastContainer>
+      <CompleteButton text="작성완료" onPress={goToNextPage}></CompleteButton>
     </Container>
   );
 };
 
-export default RediaryWriteScreen;
+export default EmotionScreen;
 
 const Container = styled.ScrollView`
   flex: 1;
   background-color: ${colors.palette.White};
+`;
+
+const ToastContainer = styled.View`
+  position: absolute;
+  bottom: 40px;
+  left: 0;
+  right: 0;
+  align-items: flex-end;
 `;
 
 const TitleText = styled.Text`
