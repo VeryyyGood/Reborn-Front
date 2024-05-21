3c31436d7d17e0a7a6615a04ba1b92f8984749d6
kimbodle <103980156+kimbodle@users.noreply.github.com>
2024-05-21 16:12:26 +0900

    DeviceToken

--- Reborn/App.js
+++ Reborn/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Root from "./Navigation/Root";
+import messaging from '@react-native-firebase/messaging';
 
 import AppContext from "./Screens/RebornScreens/dog/AppContext";
 import {
@@ -9,7 +10,30 @@ import {
   GlobalPetNameProvider,
 } from "./context/AccessTokenContext";
 
+
 export default function App() {
+  
+  async function requestUserPermission() {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+  
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  }
+  
+  async function getToken() {
+    await requestUserPermission();
+    const token = await messaging().getToken();
+    console.log('Device Token:', token);
+    return token;
+  }
+  
+  // 앱이 실행될 때 토큰을 얻기 위해 함수 호출
+  getToken();
+  
   const [contentsDay, setContentsDay] = useState(1);
 
   const setDay = (day) => {
